4b67587497ac8060e03f0098fe3f03822fbbc677
ArvidLW <lw_coming@163.com>
2017-03-24 12:08:11 +0800

    增加单纯形线性规划

---
+++ lwlp.cpp
@@ -0,0 +1,20 @@
+//
+// Created by lw_co on 2017/3/21.
+//
+
+#include <vector>
+#include "lwlp.h"
+
+//matrixC的前k个为基向量，k为matrix列数，后n-1-k个为非基变量，最后一列为资源向量b
+int lwlp(std::vector<std::vector<double > > matrixC,std::vector<double > vectorO){
+    //int m=matrixC.size();//基变量个数
+    //int n=vectorO.size();//矩阵列数减1，不包含资源向量
+    //LinearRe re(&matrixC,&vectorO);
+    //re.cycleRun();
+    //第一次迭代
+
+    //int k=
+    //构造可行基
+    //计算检验数
+    return 0;
+}
